add4cc1fdfd8331a399ce23e40de473386aeef95
chenzhiling <122769965@qq.com>
2024-05-29 17:25:16 +0800

    前端框架-添加pinia

--- czl-console/czl-console-ui/src/main.ts
+++ czl-console/czl-console-ui/src/main.ts
@@ -8,8 +8,11 @@ import router from "./router";
 //导入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import {createPinia} from "pinia";
 
 createApp(App)
     .use(router)
+    //使用Pinia
+    .use(createPinia())
     .use(ElementPlus)
     .mount('#app')
