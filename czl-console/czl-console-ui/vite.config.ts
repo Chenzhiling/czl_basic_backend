00f258f8ed4b71ef9b66eb4e7b35666b56ea1868
chenzhiling <122769965@qq.com>
2024-05-29 17:24:26 +0800

    前端框架-设置代理

--- czl-console/czl-console-ui/vite.config.ts
+++ czl-console/czl-console-ui/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true,
+    // https: true,
+    // 设置代理，根据我们项目实际情况配置
+    proxy: {
+      '/api': {
+        target: 'http://10.88.10.44:1202/chaoyi',
+        // changeOrigin: true,
+        ws: true,
+        // secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
